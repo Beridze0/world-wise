6516cf88a176ce7211185dfaccf5f1b01a18e74e
Beridze0 <beridzedavit904@gmail.com>
2025-07-07 13:58:18 +0400

    builidng appComponent

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,10 +1,12 @@
-import AppNav from "../components/AppNav";
+import Map from "../components/Map";
+import Sidebar from "../components/Sidebar";
+import styles from "./AppLayout.module.css";
 
 export default function AppLayout() {
   return (
-    <nav>
-      <AppNav />
-      <p>APP</p>
-    </nav>
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
+    </div>
   );
 }
